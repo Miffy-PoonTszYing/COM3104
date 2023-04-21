1fdc497a2004c756bd35cdd4587d20e06f040ff2
JeremyIn <jeremyGitHub0208@gmail.com>
2023-04-21 23:11:58 +0800

    Update on the Calculation(MainActivity)

--- app/src/main/java/com/example/myapplication/Account.java
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -16,10 +16,9 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class Account extends AppCompatActivity {
-    ImageButton bt_add , bt_wallet ,bt_home, bt_plan, bt_account ;
-    Button bt_display;
-    ListView lv_show;
     DBOpenHelper myDb;
+    ImageButton bt_add , bt_wallet ,bt_home, bt_plan, bt_account ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +30,6 @@ public class Account extends AppCompatActivity {
         bt_home = findViewById(R.id.bt_home);
         bt_plan = findViewById(R.id.bt_plan);
         bt_account = findViewById(R.id.bt_account);
-        lv_show = findViewById(R.id.lv_show);
-        bt_display = findViewById(R.id.bt_display);
 
         bt_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,41 +72,6 @@ public class Account extends AppCompatActivity {
             }
         });
 
-        bt_display.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Cursor cursor= myDb.getAllData(); // get the query result from db and save it in a Cursor object
-                int resultCounts = cursor.getCount();
-                // cursor.moveToFirst returns false if the cursor is empty
-                if (resultCounts == 0 || !cursor.moveToFirst()){
-                    Toast.makeText(Account.this, "no data", Toast.LENGTH_LONG).show();
-                }else{
-                    // in a for loop get the name and number for each contact
-                    // merge the info into one string
-                    // append the string to an arraylist
-                    int index_amount = 0;
-                    int index_category = 0;
-                    ArrayList contacts = new ArrayList();
-                    String str_row = null;
-                    for(int i=0; i<resultCounts;i++){
-                        index_amount = cursor.getColumnIndex(DBOpenHelper.KEY_Amount);
-                        index_category = cursor.getColumnIndex(DBOpenHelper.KEY_Category);
-                        str_row = cursor.getString(index_amount)+"\n"+cursor.getInt(index_category);
-                        Log.d("DB: ", "row "+i+" = "+ str_row);
-                        contacts.add(str_row);
-                        cursor.moveToNext();
-                    }
 
-                    // standard use of ListView
-                    // build an adapter with the built-in layout simple_list_item_1
-                    // bind the layout with the dataset
-                    // bind your adapter to ListView lv_show
-                    ArrayAdapter adapter = new ArrayAdapter(Account.this,
-                            android.R.layout.simple_list_item_1, contacts);
-                    lv_show.setAdapter(adapter);
-                }
-            }
-        });
     }
-
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,12 +13,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton bt_add , bt_wallet ,bt_home, bt_plan, bt_account ;
+    DBOpenHelper myDb;
+    TextView tv_total;
+    ListView lv_summary;
 
 
 
@@ -31,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         bt_home = findViewById(R.id.bt_home);
         bt_plan = findViewById(R.id.bt_plan);
         bt_account = findViewById(R.id.bt_account);
+        tv_total = findViewById(R.id.tv_total);
+        lv_summary = findViewById(R.id.lv_summary);
 
 
         bt_add.setOnClickListener(new OnClickListener() {
@@ -70,8 +77,80 @@ public class MainActivity extends AppCompatActivity {
         });
         /******************* End OnClickListener for mean bar********************************/
 
+        // Calculation of the expense
+        myDb = new DBOpenHelper(this);
+        Cursor cursor= myDb.getAllData();
+        int resultCounts = cursor.getCount();
 
+        if (resultCounts == 0 || !cursor.moveToFirst()){
+            Toast.makeText(MainActivity.this, "no data", Toast.LENGTH_LONG).show();
+        }else{
+            int index_type = 0;
+            int index_amount = 0;
+            int index_date = 0;
+            int index_category = 0;
+            ArrayList record = new ArrayList();
+            //Output value
+            int total = 0;
+            Log.d("DB: ", "Total"+ total);
+            int Food = 0;
+            Log.d("DB: ", "Food Expense"+ Food);
+            int Transportation = 0;
+            Log.d("DB: ", "Transportation Expense"+ Transportation);
+            int Bill = 0;
+            Log.d("DB: ", "Bill"+ Bill);
+            int Pet = 0;
+            Log.d("DB: ", "Pet Expense"+ Pet);
+            int Salary = 0;
+            Log.d("DB: ", "Salary "+ total);
+            int Gift = 0;
+            Log.d("DB: ", "Gift Expense"+ total);
+            String category = null;
 
+            for(int i=0; i<resultCounts;i++){
+                index_type = cursor.getColumnIndex(DBOpenHelper.KEY_Type);
+                index_amount = cursor.getColumnIndex(DBOpenHelper.KEY_Amount);
+                index_date = cursor.getColumnIndex(DBOpenHelper.KEY_Date);
+                index_category = cursor.getColumnIndex(DBOpenHelper.KEY_Category);
+
+                total = total + cursor.getInt(index_amount);
+                Log.d("DB: ", "total"+ i +" = "+ total);
+                category = cursor.getString(index_category);
+                switch (category){
+                    case "Food":
+                        Food = Food + cursor.getInt(index_amount);
+                        break;
+                    case "transportation":
+                        Transportation = Transportation + cursor.getInt(index_amount);
+                        break;
+                    case "Bill":
+                        Bill = Bill + cursor.getInt(index_amount);
+                        break;
+                    case "Pet":
+                        Pet = Pet + cursor.getInt(index_amount);
+                        break;
+                    case "Salary":
+                        Salary = Salary + cursor.getInt(index_amount);
+                        break;
+                    case "Gift":
+                        Gift = Gift + cursor.getInt(index_amount);
+                        break;
+                }
+                cursor.moveToNext();
+
+            }
+            record.add("Food Expense = " + Food);
+            record.add("Transportation Expense = "+ Transportation);
+            record.add("Bill = "+ Bill);
+            record.add("Pet Expense = "+ Pet);
+            record.add("Salary = "+ Salary);
+            record.add("Gift Expense = "+ Gift);
+
+            ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,
+                    android.R.layout.simple_list_item_1, record);
+            lv_summary.setAdapter(adapter);
+            tv_total.setText(Integer.toString(total));
+        }
 
     }
 }
